d8d84c2deb4c1fb235fe7bddbf0366f5902f5d44
Mehul <mehulsolanki45@gmail.com>
2016-06-16 19:36:33 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,b=0;
+    cout<<"Enter a number :"<<endl;
+    cin>>n;
+    for(int i=3;i<=n;i++){
+       for(int a=2;a<i;a++){
+            if(i%a==0)
+            b++;
+       }
+       if(b==0)
+        cout<<i<<" is a prime number "<<endl;
+
+       b=0;
+    }
+}
